7dd530a0cae6d995a6289d8700f7a9aeb221448d
abdelwareth <abdo2012_abdo2013@yahoo.com>
2017-06-15 05:47:58 +0200

    Create carnums.php

--- codes/carnums.php
+++ codes/carnums.php
@@ -11,8 +11,22 @@ $connect=mysqli_connect($localhost,$user,$pass,$db);
 $sql="SELECT * FROM ourcars ";
 $query=mysqli_query($connect,$sql);		
 $num=mysqli_num_rows($query); //يعد الصفوف الناتجه عن الكويري //
-		
-echo $num ;
+$sql1="SELECT * FROM ourcars where id='1'";
+$query1=mysqli_query($connect,$sql1);		
+ $array = array();
+ $array[0]=$num;
+
+while ($row=mysqli_fetch_array($query1)){
+$array[1] = $row['feature1'];
+$array[2] = $row['feature2'];
+$array[3] = $row['feature3'];
+$array[4] = $row['feature4'];
+$array[5] = $row['feature5'];
+$array[6] = $row['uname'];
+$array[7] = $row['carsnum'];
+			
+}		
+echo json_encode($array);
 		
 		
 		
